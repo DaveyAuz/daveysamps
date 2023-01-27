3b7c5dc3e98313148320c5072656cd37d718717e
David Jon Oswald II <doswald27@gmail.com>
2023-01-27 01:08:15 +0000

    added css, js, list, pictures, more html, changed URL, made site go live

---
+++ script.js
@@ -0,0 +1,13 @@
+<script>
+// function adTwoNumbers(){
+    let number1 = 10
+    let number2 = 20
+    console.log(number1 + number2);
+    return number1 + number2;
+    let sum = number1 + number2
+    }
+    
+    console.log()
+
+console.log (addTwoNumbers)
+</script>
