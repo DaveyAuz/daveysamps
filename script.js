979d36d4fcf499ea8ec70b3cec79e610d8433555
David Jon Oswald II <doswald27@gmail.com>
2023-01-28 19:31:01 +0000

    added 3 JS dynamic components to webpage.

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+// First Dynamic Element   
+let user = prompt("Please Enter Your Name", " ")
+
+// Second Dynamic
+
+document.write("Welcome, " + user + "!")
+
+// Third Dynamic 
+
+ans = confirm("THIS IS ABOUT TO GET LOUD... DO YOU HAVE PROPER HEARING PROTECTION??!!");
+    if (ans == true) {
+        alert("Most Excellent!")
+    }else{
+        alert("Bogus!")    
+    }
+
+
+
+
+
+
+
+
+
+// ("Well, its too late to turn back now..." + theName)
+// prompt(THIS IS ABOUT TO GET LOUD... DO YOU HAVE PROPER HEARING PROTECTION??!!)
+
 // // function functionName(parameters){code you want executed}
 // This is how I comment bruh
 // function functionName(){
@@ -5,11 +32,7 @@
 // } 
 // let theName = '';
 
-Function greetUser(){
-     = prompt(THIS IS ABOUT TO GET LOUD... DO YOU HAVE PROPER HEARING PROTECTION??!!)
-    console.log(theName); 
-    document.write("Well, its too late to turn back now..."); 
-}
+
 //    theName
 //alert()
 
